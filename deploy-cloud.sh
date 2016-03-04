b72a2f5e151694a49c9c0024f36e52df5f8195a0
kiwenlau <kiwenlau@126.com>
2016-03-04 18:46:06 +0900

    no memory and network

--- deploy-cloud.sh
+++ deploy-cloud.sh
@@ -7,9 +7,14 @@ SLAVE_IP=(192.168.59.62 192.168.59.63 192.168.59.64 192.168.59.65 192.168.59.66)
 # sudo mkdir -p /var/lib/ganglia/rrds
 # sudo chown -R 999:999 /var/lib/ganglia
 
-
-echo -e "\nstart master node..."
+# delete master and slave containers
 sudo docker rm -f ganglia-master > /dev/null
+for (( i = 0; i < 5; i++ )); do
+         sudo docker -H tcp://${SLAVE_IP[$i]}:4000 rm -f ganglia-slave$i > /dev/null
+done
+
+
+echo -e "\nstart ganglia-master..."
 sudo docker run -d --net=host --name ganglia-master \
                 -v /etc/localtime:/etc/localtime:ro \
                 -e MASTER_IP=$MASTER_IP \
@@ -18,10 +23,9 @@ sudo docker run -d --net=host --name ganglia-master \
 
 
 # start slave container
-for (( i = 0; i < 5; i++ )); do
-         sudo docker -H tcp://${SLAVE_IP[$i]}:4000 rm -f ganglia-slave$i > /dev/null
-         echo -e "\nstart slave$i container..."
-         sudo docker -H tcp://${SLAVE_IP[$i]}:4000 run -d --net=host --name ganglia-slave$i \
+for (( i = 0; i < 1; i++ )); do
+         echo -e "\nstart ganglia slave$i..."
+         sudo docker -H tcp://${SLAVE_IP[$i]}:4000 run -d --privileged --net=host --name ganglia-slave$i \
                                                        -v /etc/localtime:/etc/localtime:ro \
                                                        -e MASTER_IP=$MASTER_IP \
                                                        kiwenlau/ganglia \
