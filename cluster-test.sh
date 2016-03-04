2bd2292d95d97f632f463f91c5ceae61780c3831
kiwenlau <kiwenlau@126.com>
2016-03-04 16:17:34 +0900

    set the MASTER_IP dynamically

--- cluster-test.sh
+++ cluster-test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+MASTER_IP=192.168.59.1
 SLAVE_IP=(192.168.59.2)
 
-sudo rm -rf /var/lib/ganglia
-sudo mkdir -p /var/lib/ganglia/rrds
-sudo chown -R 999:999 /var/lib/ganglia
+# sudo rm -rf /var/lib/ganglia
+# sudo mkdir -p /var/lib/ganglia/rrds
+# sudo chown -R 999:999 /var/lib/ganglia
 
  # start slave container
 for (( i = 0; i < 1; i++ )); do
@@ -12,6 +13,7 @@ for (( i = 0; i < 1; i++ )); do
          echo -e "\nstart slave$i container..."
          sudo docker -H tcp://${SLAVE_IP[$i]}:4000 run -d --net=host --name gangalia-slave$i \
                                                        -v /etc/localtime:/etc/localtime:ro \
+                                                       -e MASTER_IP=$MASTER_IP \
                                                        kiwenlau/ganglia \
                                                        supervisord --configuration=/etc/supervisor/conf.d/ganglia-slave.conf > /dev/null
 done
@@ -20,6 +22,7 @@ echo -e "\nstart master node..."
 sudo docker rm -f $(sudo docker ps -aq) > /dev/null
 sudo docker run -d --net=host --name ganglia-master \
                 -v /etc/localtime:/etc/localtime:ro \
+                -e MASTER_IP=$MASTER_IP \
                 kiwenlau/ganglia \
                 supervisord --configuration=/etc/supervisor/conf.d/ganglia-master.conf > /dev/null
 
