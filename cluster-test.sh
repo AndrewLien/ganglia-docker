2353ca9f33962139d1fd6787b48b4a591d2c7a82
kiwenlau <kiwenlau@126.com>
2016-03-04 18:01:44 +0900

    see memory and network metrics

--- cluster-test.sh
+++ cluster-test.sh
@@ -7,6 +7,14 @@ SLAVE_IP=(192.168.59.2)
 # sudo mkdir -p /var/lib/ganglia/rrds
 # sudo chown -R 999:999 /var/lib/ganglia
 
+echo -e "\nstart master node..."
+sudo docker rm -f $(sudo docker ps -aq) > /dev/null
+sudo docker run -d --net=host --name ganglia-master \
+                -v /etc/localtime:/etc/localtime:ro \
+                -e MASTER_IP=$MASTER_IP \
+                kiwenlau/ganglia \
+                supervisord --configuration=/etc/supervisor/conf.d/ganglia-master.conf > /dev/null
+
  # start slave container
 for (( i = 0; i < 1; i++ )); do
          sudo docker -H tcp://${SLAVE_IP[$i]}:4000 rm -f $(sudo docker -H tcp://${SLAVE_IP[$i]}:4000 ps -aq) > /dev/null
@@ -17,14 +25,6 @@ for (( i = 0; i < 1; i++ )); do
                                                        kiwenlau/ganglia \
                                                        supervisord --configuration=/etc/supervisor/conf.d/ganglia-slave.conf > /dev/null
 done
-
-echo -e "\nstart master node..."
-sudo docker rm -f $(sudo docker ps -aq) > /dev/null
-sudo docker run -d --net=host --name ganglia-master \
-                -v /etc/localtime:/etc/localtime:ro \
-                -e MASTER_IP=$MASTER_IP \
-                kiwenlau/ganglia \
-                supervisord --configuration=/etc/supervisor/conf.d/ganglia-master.conf > /dev/null
 
 
 
