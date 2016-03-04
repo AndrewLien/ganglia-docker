3f4ed87e0f2dbdefa372a2c6614839ed62b33322
kiwenlau <kiwenlau@126.com>
2016-03-04 10:18:21 +0900

    using single image

---
+++ backup/local-test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo -e "\nbuild gangalia master image...\n"
+sudo docker build -t kiwenlau/gangalia-master master/.
+
+echo -e "\nbuild gangalia slave image...\n"
+sudo docker build -t kiwenlau/gangalia-slave slave/.
+
+
+sudo docker rm -f $(sudo docker ps -aq) > /dev/null
+
+echo -e "\nstart slave node..."
+sudo docker run -d -p 8649:8649/udp --name slave kiwenlau/gangalia-slave > /dev/null
+
+CURRENT_DIRECTORY=`pwd`
+
+sudo sh -c "cat > gmetad.conf <<EOL
+data_source \"cluster1\" slave:8649
+setuid_username \"ganglia\"
+case_sensitive_hostnames 0
+EOL"
+
+
+echo -e "\nstart master node..."
+sudo docker run -d --name master \
+                -v /var/lib/ganglia:/var/lib/ganglia \
+                -v $CURRENT_DIRECTORY/gmetad.conf:/etc/ganglia/gmetad.conf \
+                --link slave:slave  \
+                -p 80:80 \
+                kiwenlau/gangalia-master > /dev/null
+
+
+echo ""
--- local-test.sh
+++ local-test.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
-echo -e "\nbuild gangalia master image...\n"
-sudo docker build -t kiwenlau/gangalia-master master/.
-
-echo -e "\nbuild gangalia slave image...\n"
-sudo docker build -t kiwenlau/gangalia-slave slave/.
-
+echo -e "\nbuild ganglia image...\n"
+sudo docker build -t kiwenlau/ganglia ganglia/.
 
 sudo docker rm -f $(sudo docker ps -aq) > /dev/null
 
-echo -e "\nstart slave node..."
-sudo docker run -d -p 8649:8649/udp --name slave kiwenlau/gangalia-slave > /dev/null
-
 CURRENT_DIRECTORY=`pwd`
 
+echo -e "\nstart slave node..."
+sudo docker run -d -p 8649:8649/udp --name ganglia-slave \
+     -v $CURRENT_DIRECTORY/gmetad.conf:/etc/ganglia/gmetad.conf \
+     kiwenlau/ganglia \
+     supervisord --configuration=/etc/supervisor/conf.d/ganglia-slave.conf > /dev/null
+
 sudo sh -c "cat > gmetad.conf <<EOL
-data_source \"cluster1\" slave:8649
+data_source \"cluster1\" ganglia-slave:8649
 setuid_username \"ganglia\"
 case_sensitive_hostnames 0
 EOL"
 
 
 echo -e "\nstart master node..."
-sudo docker run -d --name master \
+sudo docker run -d --name ganglia-master \
                 -v /var/lib/ganglia:/var/lib/ganglia \
                 -v $CURRENT_DIRECTORY/gmetad.conf:/etc/ganglia/gmetad.conf \
-                --link slave:slave  \
+                --link ganglia-slave:ganglia-slave  \
                 -p 80:80 \
-                kiwenlau/gangalia-master > /dev/null
+                kiwenlau/ganglia \
+                supervisord --configuration=/etc/supervisor/conf.d/ganglia-master.conf > /dev/null
+                > /dev/null
 
 
 echo ""
