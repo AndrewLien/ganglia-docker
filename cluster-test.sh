1ce7f30062f6b50035a5f1c1aa3c6dcba3b49548
kiwenlau <kiwenlau@126.com>
2016-03-04 11:17:34 +0900

    can not see slave

--- cluster-test.sh
+++ cluster-test.sh
@@ -3,10 +3,14 @@
 MASTER_IP=192.168.59.1
 SLAVE_IP=(192.168.59.2)
 
+sudo rm -rf /var/lib/ganglia
+sudo mkdir -p /var/lib/ganglia/rrds
+sudo chown -R 999:999 /var/lib/ganglia
 
 echo -e "\nstart master node..."
 sudo docker rm -f $(sudo docker ps -aq) > /dev/null
 sudo docker run -d --net=host --name ganglia-master \
+                -v /etc/localtime:/etc/localtime:ro \
                 -v /var/lib/ganglia:/var/lib/ganglia \
                 kiwenlau/ganglia \
                 supervisord --configuration=/etc/supervisor/conf.d/ganglia-master.conf > /dev/null
@@ -16,6 +20,7 @@ for (( i = 0; i < 1; i++ )); do
         sudo docker -H tcp://${SLAVE_IP[$i]}:4000 rm -f $(sudo docker -H tcp://${SLAVE_IP[$i]}:4000 ps -aq) > /dev/null
         echo -e "\nstart slave$i container..."
         sudo docker -H tcp://${SLAVE_IP[$i]}:4000 run -d --net=host --name gangalia-slave$i \
+                                                      -v /etc/localtime:/etc/localtime:ro \
                                                       kiwenlau/ganglia \
                                                       supervisord --configuration=/etc/supervisor/conf.d/ganglia-slave.conf > /dev/null
 done
