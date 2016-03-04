6560208bd61bafe7c2e778516a16e98a41d23033
kiwenlau <kiwenlau@126.com>
2016-03-04 12:53:24 +0900

    run single node successfully

--- cluster-test.sh
+++ cluster-test.sh
@@ -7,15 +7,15 @@ sudo rm -rf /var/lib/ganglia
 sudo mkdir -p /var/lib/ganglia/rrds
 sudo chown -R 999:999 /var/lib/ganglia
 
-# start slave container
-for (( i = 0; i < 1; i++ )); do
-        sudo docker -H tcp://${SLAVE_IP[$i]}:4000 rm -f $(sudo docker -H tcp://${SLAVE_IP[$i]}:4000 ps -aq) > /dev/null
-        echo -e "\nstart slave$i container..."
-        sudo docker -H tcp://${SLAVE_IP[$i]}:4000 run -d --net=host --name gangalia-slave$i \
-                                                      -v /etc/localtime:/etc/localtime:ro \
-                                                      kiwenlau/ganglia \
-                                                      supervisord --configuration=/etc/supervisor/conf.d/ganglia-slave.conf > /dev/null
-done
+# # start slave container
+# for (( i = 0; i < 1; i++ )); do
+#         sudo docker -H tcp://${SLAVE_IP[$i]}:4000 rm -f $(sudo docker -H tcp://${SLAVE_IP[$i]}:4000 ps -aq) > /dev/null
+#         echo -e "\nstart slave$i container..."
+#         sudo docker -H tcp://${SLAVE_IP[$i]}:4000 run -d --net=host --name gangalia-slave$i \
+#                                                       -v /etc/localtime:/etc/localtime:ro \
+#                                                       kiwenlau/ganglia \
+#                                                       supervisord --configuration=/etc/supervisor/conf.d/ganglia-slave.conf > /dev/null
+# done
 
 echo -e "\nstart master node..."
 sudo docker rm -f $(sudo docker ps -aq) > /dev/null
