88ffc3287c68c7bd2326afdf07c33076aaaf317e
kiwenlau <kiwenlau@126.com>
2016-03-06 01:16:03 +0900

    backup

--- deploy-cloud.sh
+++ deploy-cloud.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-MASTER_IP=192.168.59.156
+#kai-galaxy-aurora2-A
+#MASTER_IP=192.168.59.156
+
+#kai-node1
+MASTER_IP=192.168.59.62
 
 # kai-node: Docker in Docker and Sibling Docker
 #SLAVE_IP=(192.168.59.62 192.168.59.63 192.168.59.64 192.168.59.65 192.168.59.66)
@@ -9,7 +13,10 @@ MASTER_IP=192.168.59.156
 #SLAVE_IP=(192.168.59.15 192.168.59.16 192.168.59.17 192.168.59.18 192.168.59.19)
 
 # kai-kubernetes2: Tool in Docker
-SLAVE_IP=(192.168.59.85 192.168.59.86 192.168.59.87 192.168.59.88 192.168.59.89)
+#SLAVE_IP=(192.168.59.85 192.168.59.86 192.168.59.87 192.168.59.88 192.168.59.89)
+
+# kai-aurora2: Tool in Docker
+SLAVE_IP=(192.168.59.156 192.168.59.157 192.168.59.158 192.168.59.159 192.168.59.160)
 
 # sudo rm -rf /var/lib/ganglia
 # sudo mkdir -p /var/lib/ganglia/rrds
